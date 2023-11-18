9b14b25e014e3e30c1e4708590b19044fb10e11f
add19 <aadish.deshpande@gmail.com>
2023-11-17 19:50:12 -0500

    corrected lb loop

--- lb.go
+++ lb.go
@@ -29,77 +29,39 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	serverEndpoint := lbObject.servers[lbObject.index%len(lbObject.servers)]
 	lbObject.index++
 
-	for range lbObject.servers {
-		wg.Add(1)
-		go func(endpoint string) {
-			defer wg.Done()
-			request, err := http.NewRequest("GET", endpoint, nil)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
+	wg.Add(1)
+	go func(endpoint string) {
+		defer wg.Done()
+		request, err := http.NewRequest("GET", endpoint, nil)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
 
-			response, err := lbObject.client.Do(request)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			defer response.Body.Close()
+		response, err := lbObject.client.Do(request)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer response.Body.Close()
 
-			responseBody, err := io.ReadAll(response.Body)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
+		responseBody, err := io.ReadAll(response.Body)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
 
-			mu.Lock()
-			defer mu.Unlock()
-			responses = append(responses, fmt.Sprintf("Received request from %s \nGET / HTTP/1.1\nHost: %s\nUser-Agent: %s\nAccept: */*\n%s", r.RemoteAddr, r.Host, r.UserAgent(), responseBody))
-		}(serverEndpoint)
-	}
+		mu.Lock()
+		defer mu.Unlock()
+		responses = append(responses, fmt.Sprintf("Received request from %s \nGET / HTTP/1.1\nHost: %s\nUser-Agent: %s\nAccept: */*\n%s", r.RemoteAddr, r.Host, r.UserAgent(), responseBody))
+	}(serverEndpoint)
 
 	wg.Wait()
 
-	// Writing collected responses to the ResponseWriter outside of goroutines
 	for _, resp := range responses {
 		fmt.Fprintf(w, "%s\n", resp)
 	}
 }
-
-// func handler(w http.ResponseWriter, r *http.Request) {
-// 	w.Header().Set("Content-Type", "application/json")
-
-// 	var wg sync.WaitGroup
-// 	serverEndpoint := lbObject.servers[lbObject.index%len(lbObject.servers)]
-// 	lbObject.index++
-
-// 	for range lbObject.servers {
-// 		wg.Add(1)
-// 		go func(serverEndpoint string) {
-// 			defer wg.Done()
-// 			request, err := http.NewRequest("GET", serverEndpoint, nil)
-
-// 			if err != nil {
-// 				fmt.Println(err)
-// 				return
-// 			}
-
-// 			response, error := lbObject.client.Do(request)
-
-// 			if error != nil {
-// 				fmt.Println(error)
-// 			}
-// 			responseBody, error := io.ReadAll(response.Body)
-
-// 			if error != nil {
-// 				fmt.Println(error)
-// 			}
-// 			defer response.Body.Close()
-// 			fmt.Fprintf(w, "Received request from %s \nGET / HTTP/1.1\nHost: %s\nUser-Agent: %s\nAccept: */*\n%s", r.RemoteAddr, r.Host, r.UserAgent(), responseBody)
-// 		}(serverEndpoint)
-// 	}
-// 	wg.Wait()
-// }
 
 func main() {
 	http.HandleFunc("/", handler)
