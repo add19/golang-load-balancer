d96af1e21e11d996e1a9229ee9bda084dcc6f48c
add19 <aadish.deshpande@gmail.com>
2023-11-17 19:43:07 -0500

    round robin implementation with corrections in concurrency

--- lb.go
+++ lb.go
@@ -5,34 +5,101 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"sync"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	go func() {
-		w.Header().Set("Content-Type", "application/json")
-
-		apiUrl := "http://127.0.0.1:8080/"
-		request, err := http.NewRequest("GET", apiUrl, nil)
-
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		client := &http.Client{}
-		response, error := client.Do(request)
-
-		if error != nil {
-			fmt.Println(error)
-		}
-		responseBody, error := io.ReadAll(response.Body)
-
-		if error != nil {
-			fmt.Println(error)
-		}
-		fmt.Fprintf(w, "Received request from %s \nGET / HTTP/1.1\nHost: %s\nUser-Agent: %s\nAccept: */* \n%s", r.RemoteAddr, r.Host, r.UserAgent(), responseBody)
-	}()
+type RoundRobinScheduler struct {
+	index   int
+	servers []string
+	client  *http.Client
 }
+
+var lbObject = RoundRobinScheduler{
+	index:   0,
+	servers: []string{"http://127.0.0.1:8080/", "http://127.0.0.1:8081/", "http://127.0.0.1:8082/"},
+	client:  &http.Client{},
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+	var responses []string
+	serverEndpoint := lbObject.servers[lbObject.index%len(lbObject.servers)]
+	lbObject.index++
+
+	for range lbObject.servers {
+		wg.Add(1)
+		go func(endpoint string) {
+			defer wg.Done()
+			request, err := http.NewRequest("GET", endpoint, nil)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			response, err := lbObject.client.Do(request)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			defer response.Body.Close()
+
+			responseBody, err := io.ReadAll(response.Body)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			mu.Lock()
+			defer mu.Unlock()
+			responses = append(responses, fmt.Sprintf("Received request from %s \nGET / HTTP/1.1\nHost: %s\nUser-Agent: %s\nAccept: */*\n%s", r.RemoteAddr, r.Host, r.UserAgent(), responseBody))
+		}(serverEndpoint)
+	}
+
+	wg.Wait()
+
+	// Writing collected responses to the ResponseWriter outside of goroutines
+	for _, resp := range responses {
+		fmt.Fprintf(w, "%s\n", resp)
+	}
+}
+
+// func handler(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type", "application/json")
+
+// 	var wg sync.WaitGroup
+// 	serverEndpoint := lbObject.servers[lbObject.index%len(lbObject.servers)]
+// 	lbObject.index++
+
+// 	for range lbObject.servers {
+// 		wg.Add(1)
+// 		go func(serverEndpoint string) {
+// 			defer wg.Done()
+// 			request, err := http.NewRequest("GET", serverEndpoint, nil)
+
+// 			if err != nil {
+// 				fmt.Println(err)
+// 				return
+// 			}
+
+// 			response, error := lbObject.client.Do(request)
+
+// 			if error != nil {
+// 				fmt.Println(error)
+// 			}
+// 			responseBody, error := io.ReadAll(response.Body)
+
+// 			if error != nil {
+// 				fmt.Println(error)
+// 			}
+// 			defer response.Body.Close()
+// 			fmt.Fprintf(w, "Received request from %s \nGET / HTTP/1.1\nHost: %s\nUser-Agent: %s\nAccept: */*\n%s", r.RemoteAddr, r.Host, r.UserAgent(), responseBody)
+// 		}(serverEndpoint)
+// 	}
+// 	wg.Wait()
+// }
 
 func main() {
 	http.HandleFunc("/", handler)
--- server/server.go
+++ server/server.go
@@ -17,34 +17,3 @@ func main() {
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
-
-// func handle(conn net.Conn) {
-// 	remoteHost, _ := net.LookupAddr(strings.Split(conn.RemoteAddr().String(), ":")[0])
-// 	var b bytes.Buffer
-// 	fmt.Fprintf(&b, "Received request from %s \nGET / HTTP/1.1\nHost: %s\nUser-Agent: %s\nAccept: */*\n\nReplied with a hello message", conn.RemoteAddr(), remoteHost, conn.LocalAddr())
-
-// 	_, err := conn.Write(b.Bytes())
-// 	if err != nil {
-// 		fmt.Println("Error:", err)
-// 		return
-// 	}
-// }
-
-// func main() {
-// 	listener, err := net.Listen("tcp", "localhost:8080")
-// 	if err != nil {
-// 		fmt.Println("Error: ", err)
-// 		return
-// 	}
-// 	defer listener.Close()
-
-// 	for {
-// 		conn, err := listener.Accept()
-// 		if err != nil {
-// 			fmt.Println("Error: ", err)
-// 			continue
-// 		}
-
-// 		go handle(conn)
-// 	}
-// }
